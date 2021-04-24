bccf4c6efeea1e62c81d2bb51935e2fcb1b1031b
Subha chakraborty <63599774+subhachakraborty@users.noreply.github.com>
2021-04-24 18:06:12 +0530

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -4,4 +4,5 @@ tip_percentage = int(input("what percentage of tip you like to give? 10,12 or 15
 people_no = int(input("How many people to split the bill? "))
 pay = (total_bill + total_bill*tip_percentage/100 )/people_no
 print(f"Each person should pay: {round(pay,2)}")
+#or you can write 
 #print("Each person should pay:",round(pay,2))
